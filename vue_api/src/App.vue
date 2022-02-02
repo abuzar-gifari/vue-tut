58f54826f0879ecb9ff25899690daffbb24db754
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-02-02 22:06:25 +0600

    vue-api created

--- vue_api/src/App.vue
+++ vue_api/src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>User's data</h1>
+  <ul class="item" v-for="item in list" :key="item.id">
+    <li>{{ item.id }}</li>
+    <li>{{ item.email }}</li>
+    <li>{{ item.first_name }}</li>
+    <li><img :src="item.avatar"></li>
+  </ul>
+
+
+<!-- send data to API with post method -->
+
+  <input type="text" name="email" v-model="email"><br>
+  <input type="text" name="password" v-model="password">
+  <button v-on:click="addUser()">Enter new user</button>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      list:[],
+      email:'',
+      password:''
+    }
+  },
+  methods:{
+    async addUser(){
+      let result= await axios.post("http://localhost:8080/user",{
+        email:this.email,
+        password:this.password
+      })
+      console.warn(result);
+    }
+  },
+  async mounted(){
+      let result = await axios.get("https://reqres.in/api/users?page=1");
+      console.warn(result.data.data);
+      this.list=result.data.data;
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.item{
+
+}
+.item li{
+  display: inline-block;
+  border: 1px solid;
+  width:180px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 5px;
+}
+
+.item img{
+  width:50px;
 }
 </style>
